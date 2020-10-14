dfe273b404fed6513ec68c292b9148d0d01515d2
yiquan.ang <yiquan.ang@tngdigital.com.my>
2020-10-14 14:33:30 +0800

    feat: add signature field

--- sentry_dingtalk/plugin.py
+++ sentry_dingtalk/plugin.py
@@ -8,7 +8,12 @@ from sentry.utils import json
 from sentry.utils.http import absolute_uri
 from sentry.integrations import FeatureDescription, IntegrationFeatures
 from sentry_plugins.base import CorePluginMixin
-
+# for dingtalk signature
+import time
+import hmac
+import hashlib
+import base64
+import urllib
 
 class DingtalkPlugin(CorePluginMixin, notify.NotificationPlugin):
     title = "Sentry Dingtalk Bot"
@@ -16,8 +21,8 @@ class DingtalkPlugin(CorePluginMixin, notify.NotificationPlugin):
     description = "Post notifications to a Dingtalk webhook."
     conf_key = "sentry-dingtalk"
     required_field = "webhook"
-    author = 'Ang Yi Quan'
-    author_url = 'https://github.com/lostncg/sentry-dingtalk'
+    author = "Ang Yi Quan"
+    author_url = "https://github.com/lostncg/sentry-dingtalk"
     version = "1.0.0"
     feature_descriptions = [
         FeatureDescription(
@@ -27,6 +32,10 @@ class DingtalkPlugin(CorePluginMixin, notify.NotificationPlugin):
             """,
             IntegrationFeatures.ALERT_RULE,
         )
+    ]
+    resource_links = [
+        ("Report Issue", "https://github.com/lostncg/sentry-dingtalk/issues"),
+        ("View Source", "https://github.com/lostncg/sentry-dingtalk"),
     ]
 
     def is_configured(self, project):
@@ -38,17 +47,24 @@ class DingtalkPlugin(CorePluginMixin, notify.NotificationPlugin):
                 "name": "webhook",
                 "label": "Webhook URL",
                 "type": "url",
-                "placeholder": "https://oapi.dingtalk.com/robot/send?access_token=abcdefg",
+                "placeholder": "https://oapi.dingtalk.com/robot/send?access_token=**********",
                 "required": True,
                 "help": "Your custom dingding webhook URL.",
             },
             {
-                "name": "custom_message",
-                "label": "Custom Message",
+                "name": "custom_keyword",
+                "label": "Custom Keyword",
                 "type": "string",
-                "placeholder": "e.g. Hey <!everyone> there is something wrong",
+                "placeholder": "e.g. [Sentry] Error title",
                 "required": False,
-                "help": "Optional - dingding message formatting can be used",
+                "help": "Optional - A custom keyword as the prefix of the event title",
+            },
+            {
+                "name": "signature",
+                "label": "Additional Signature",
+                "type": "string",
+                "required": False,
+                "help": "Optional - Attach Dingtalk webhook signature to the request headers.",
             },
             {
                 "name": "include_tags",
@@ -63,7 +79,8 @@ class DingtalkPlugin(CorePluginMixin, notify.NotificationPlugin):
                 "type": "string",
                 "required": False,
                 "help": (
-                    "Only include these tags (comma separated list). " "Leave empty to include all."
+                    "Only include these tags (comma separated list). "
+                    "Leave empty to include all."
                 ),
             },
             {
@@ -81,14 +98,14 @@ class DingtalkPlugin(CorePluginMixin, notify.NotificationPlugin):
             return ()
 
         return (
-            (tagstore.get_tag_key_label(k), tagstore.get_tag_value_label(k, v)) for k, v in tag_list
+            (tagstore.get_tag_key_label(k), tagstore.get_tag_value_label(k, v))
+            for k, v in tag_list
         )
 
-    def get_tag_list(self, name, project):
-        option = self.get_option(name, project)
-        if not option:
+    def get_tag_list(self, keys):
+        if not keys:
             return None
-        return set(tag.strip().lower() for tag in option.split(","))
+        return set(tag.strip().lower() for tag in keys.split(","))
 
     def notify(self, notification, raise_exception=False):
         event = notification.event
@@ -99,16 +116,31 @@ class DingtalkPlugin(CorePluginMixin, notify.NotificationPlugin):
             return
 
         project_name = project.get_full_name().encode("utf-8")
+        webhookUrl = self.get_option("webhook", project)
+        custom_keyword = self.get_option("custom_keyword", project)
+        signature = self.get_option("signature", project)
+        include_tags = self.get_option("include_tags", project)
+        included_tag_keys = self.get_option("included_tag_keys", project)
+        include_rules = self.get_option("include_rules", project)
 
         # title
         title = event.title.encode("utf-8")
-        if self.get_option("custom_message", project):
-            title = u"{} ({})".format(
-                title, self.get_option("custom_message", project))
+        if custom_keyword:
+            title = u"[{}] {}".format(title, custom_keyword)
 
         # issue
         issue_link = group.get_absolute_url(params={"referrer": "dingding"})
         issueStr = "\n> #### [issue]({})".format(issue_link)
+
+        if signature
+            timestamp = long(round(time.time() * 1000))
+            secret = signature
+            secret_enc = bytes(secret).encode('utf-8')
+            string_to_sign = '{}\n{}'.format(timestamp, secret)
+            string_to_sign_enc = bytes(string_to_sign).encode('utf-8')
+            hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
+            sign = urllib.quote_plus(base64.b64encode(hmac_code))
+            webhookUrl = u"{}&timestamp={}&sign={}".format(webhookUrl, timestamp, sign)
 
         # 报警规则
         ruleStr = ""
@@ -121,24 +153,26 @@ class DingtalkPlugin(CorePluginMixin, notify.NotificationPlugin):
                     rule.id,
                 )
                 rule_link = absolute_uri(rule_link)
-                ruleStr = (u"\n> 由报警规则[{}]({})触发".format(
-                    rule.label, rule_link)).encode('utf-8')
+                ruleStr = (u"\n> 由报警规则[{}]({})触发".format(rule.label, rule_link)).encode(
+                    "utf-8"
+                )
 
         # 标签
         tagStr = ""
-        if self.get_option("include_tags", project):
-            included_tags = set(self.get_tag_list(
-                "included_tag_keys", project) or [])
-            excluded_tags = set(self.get_tag_list(
-                "excluded_tag_keys", project) or [])
+        if include_tags:
+            included_tags = set(self.get_tag_list(included_tag_keys) or [])
             for tag_key, tag_value in self._get_tags(event):
                 key = tag_key.lower()
                 std_key = tagstore.get_standardized_key(key)
-                if included_tags and key not in included_tags and std_key not in included_tags:
+                if (
+                    included_tags
+                    and key not in included_tags
+                    and std_key not in included_tags
+                ):
                     continue
-                tagStr = tagStr + \
-                    "\n> ###### {}:{} ".format(tag_key.encode(
-                        "utf-8"), tag_value.encode("utf-8"))
+                tagStr = tagStr + "\n> ###### {}:{} ".format(
+                    tag_key.encode("utf-8"), tag_value.encode("utf-8")
+                )
 
         payload = title
         if ruleStr:
@@ -147,12 +181,6 @@ class DingtalkPlugin(CorePluginMixin, notify.NotificationPlugin):
         if tagStr:
             payload = payload + tagStr
 
-        webhookUrl = self.get_option("webhook", project)
-        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        data = {"msgtype": "markdown",
-                "markdown": {
-                    "title": title,
-                    "text": payload
-                }
-                }
+        headers = {"Content-type": "application/json", "Accept": "text/plain"}
+        data = {"msgtype": "markdown", "markdown": {"title": title, "text": payload}}
         requests.post(webhookUrl, data=json.dumps(data), headers=headers)
